0b2fb2d89cce00d5323ae816d978757a27dd3bb1
ThatFireBoi <gcf2007@hotmail.com>
2023-09-13 13:18:56 +0000

    Modification to comply with betty character limit per line

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - Sort doubly linked list of integers in ascending order
  * @list: A pointer to the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
