2125d998d78281c7f85442c8f57f19257c98844d
ThatFireBoi <gcf2007@hotmail.com>
2023-09-13 12:51:14 +0000

    Function that sorts a doubly linked list of integers in ascending order using the insertion sort algorithm and big O notations of the time complexity of the insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * @list: A pointer to the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *insertion_point;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		insertion_point = current->prev;
+
+		while (insertion_point != NULL && insertion_point->n > current->n)
+		{
+			if (current->next != NULL)
+				current->next->prev = insertion_point;
+
+			insertion_point->next = current->next;
+			current->prev = insertion_point->prev;
+			insertion_point->prev = current;
+			current->next = insertion_point;
+
+			if (current->prev != NULL)
+				current->prev->next = current;
+			else
+				*list = current;
+
+			print_list(*list);  /* Print the list after each swap */
+
+			insertion_point = current->prev;
+		}
+
+		current = current->next;
+	}
+}
